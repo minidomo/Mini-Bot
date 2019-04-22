8df87e7e20c21eb08144a216f961b3c708b8289b
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2019-04-21 23:49:45 -0500

    refractored for v2.0.0

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
+'use strict';
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const Settings = require('./settings.json');
-const Methods = require('./methods.js');
-const CommandList = require('./commands.js').Commands;
-const prefix = '!!';
-
-const InTheChatFeature = Methods.inTheChatFeature;
+const config = require('./config');
+const mainHandler = require('./handler/main');
+const commandHandler = require('./handler/command');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('iGUMDROP', { type: 'WATCHING' });
 });
 
 client.on('message', msg => {
-    msg.content = msg.content.trim();
-    console.log(msg.createdAt + ' ' + msg.author.username + ' ' + msg.author.id);
-    if (msg.content.length > 0)
-        console.log(msg.content);
-    msg.attachments.map((val, key) => {
-        console.log(val.url);
-    });
-    if (msg.content.startsWith(prefix) && msg.author.id !== client.user.id) {
-        let contentParts = Methods.getArguments(msg.content);
-        if (!Methods.isUndefined(CommandList[contentParts.command])) {
-            CommandList[contentParts.command].execute(msg, contentParts.args);
+    mainHandler.log(msg);
+    if (!msg.author.bot)
+        if (msg.content.startsWith(config.prefix)) {
+            let obj = commandHandler.getArguments(msg, config.prefix);
+            console.log(obj);
+            let a = commandHandler.handle(msg, obj, config.commands);
+        } else {
+            mainHandler.handleFeature(msg, config.features.active);
         }
-    }
-    // if (msg.content === 'ping') {
-    //     msg.reply('pong');
-    // }
-    if (InTheChatFeature.checkFormat(msg.content)) {
-        let contentLow = msg.content.toLowerCase();
-        let lengthOfFirst = -1, indexOfLast = InTheChatFeature.getIndexOfRegex(contentLow, /\s+in\s+the\s+chat/), startingIndex = -1;
-        let firstOccurence = contentLow.match(InTheChatFeature.isARegex(contentLow) ? InTheChatFeature.canIGetARegex : InTheChatFeature.canIGetAnRegex)[0];
-        startingIndex = contentLow.indexOf(firstOccurence);
-        lengthOfFirst = firstOccurence.length;
-        if (!(lengthOfFirst === -1 || indexOfLast === -1 || startingIndex === -1)) {
-            let chant = msg.content.substring(startingIndex + lengthOfFirst, indexOfLast).trim();
-            InTheChatFeature.handleChant(msg.channel, msg.author.id, chant);
-        }
-    }
 });
 
-client.login(Settings['token']);
+client.login(config.token);
