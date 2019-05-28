73a4f3fac7f865068319042509cb126e1440ae4c
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2019-05-28 15:23:03 -0500

    output the message in rps

--- commands/rps.js
+++ commands/rps.js
@@ -6,8 +6,10 @@ const RockPaperScissors = require('../games/rockpaperscissors');
 class RPS {
     static pass(msg, args) {
         let argCheck = CommandsUtil.checkArgumentCount(args, 2, 'Not enough arguments.', 'Too many arguments.');
-        if (!argCheck.result)
+        if (!argCheck.result) {
+            msg.channel.send(argCheck.message);
             return false;
+        }
         if (!RockPaperScissors.canPlay(msg, args))
             return false;
         return true;
