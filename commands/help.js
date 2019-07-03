f82f456f53494f2bbaf093690fbb66ff8d05b7f5
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2019-07-03 18:45:34 -0500

    help command now appears when using the help command

--- commands/help.js
+++ commands/help.js
@@ -3,20 +3,6 @@
 const Discord = require('discord.js');
 const config = require('../config');
 
-// creating the help embed once to send out fast
-const fs = require('fs');
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-let description = '';
-for (const file of commandFiles) {
-    const command = require(`./${file}`);
-    if (command.visible)
-        description += `\`${command.usage}\` ${command.desc}\n`;
-}
-const helpEmbed = new Discord.RichEmbed()
-    .setColor('RED')
-    .setTitle(`Commands | Prefix: ${config.prefix}`)
-    .setDescription(description);
-
 module.exports = {
     name: 'help',
     visible: true,
@@ -30,3 +16,17 @@ module.exports = {
         msg.channel.send(helpEmbed);
     }
 };
+
+// creating the help embed once to send out fast
+const fs = require('fs');
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+let description = '';
+for (const file of commandFiles) {
+    const command = require(`./${file}`);
+    if (command.visible)
+        description += `\`${command.usage}\` ${command.desc}\n`;
+}
+const helpEmbed = new Discord.RichEmbed()
+    .setColor('RED')
+    .setTitle(`Commands | Prefix: ${config.prefix}`)
+    .setDescription(description);
