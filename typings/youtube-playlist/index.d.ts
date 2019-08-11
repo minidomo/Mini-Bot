05327b272e49a522780bcd5889c3e4db38ab6938
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2019-08-11 13:04:15 -0500

    updated typings

--- typings/youtube-playlist/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'youtube-playlist' {
-    
-    function ytlist(url: string, opts: 'id' | 'url' | 'name' | 'duration' | string[]): Promise<{ data: { playlist: object[] | string[] | number[] } }>
-
-    export = ytlist;
-}
--- typings/yt-search/index.d.ts
+++ typings/yt-search/index.d.ts
@@ -1,22 +1,55 @@
 declare module 'yt-search' {
 
     namespace ytsearch {
-        type Video = {
-            title: string,
+
+        interface MetaData {
+            title: string;
+            url: string;
             author: {
-                name: string
-            },
-            videoId: string
+                name: string,
+                id: string;
+                url?: string,
+                userId: string,
+                userName: string,
+                userUrl: string,
+                channelId: string,
+                channelName: string,
+                channelUrl: string
+            }
+        }
+
+        interface Video extends MetaData {
+            videoId: string;
+            ago: string;
+            seconds: number;
+            timestamp: string;
+            views: number;
+            duration: {
+                toString(): string;
+                seconds: number;
+                timestamp: string
+            };
+        }
+
+        type Playlist = MetaData;
+        type Account = MetaData;
+
+        type Options = {
+            query: string,
+            pageStart: number,
+            pageEnd: number
         }
 
         type Result = {
-            videos: Video[]
+            videos: Video[],
+            playlists: Playlist[],
+            accounts: Account[]
         }
     }
 
     function ytSearch(
-        query: string,
-        cb: (err: Error, result?: ytsearch.Result) => void
+        query: string | ytsearch.Options,
+        cb: (err: Error | null, result?: ytsearch.Result) => void
     ): void;
 
     export = ytSearch;
